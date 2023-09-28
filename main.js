aca0510a7ef64b8da9b6cdf7b02bfc51445a56c3
Julya Biazoto <108187223+enTrapTa06@users.noreply.github.com>
2023-09-28 17:48:55 -0300

    projeto em andamento

--- main.js
+++ main.js
@@ -9,10 +9,23 @@ for (let contador = 0; contador < listaDeTeclas.length; contador ++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`;//template string
     
     tecla.onclick = function () {
         tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code === 'Space')
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }    
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
     }
 
 }
